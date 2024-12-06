f4c9a4e8c61d24d6cb167183288341c485c1ab7f
kristinaaleee <kristinalee2000@hotmail.com>
2024-12-06 18:52:09 -0500

    Add buttons and connect to functions
    Add buttons for deleting card, read status toggle, submit/cancel for form.
    Add functions for updating page with new submissions

--- script.js
+++ script.js
@@ -5,72 +5,89 @@ function Book(title,author,pages,read) {
     this.author = author;
     this.pages = pages;
     this.read = read; 
-}
+};
+
+Book.prototype.readToggle = function(){
+    if(this.read == 'yes'){
+        this.read = 'no';
+    }
+    else{
+        this.read = 'yes';
+    }
+};
 
 function addBookToLibrary(book){
+    console.log(book);
     const card = document.createElement("div");
     card.classList.add('card');
     myLibrary.push(book);
 
     for (let key in book) {
-        const row = document.createElement('p');
-        row.classList.add('info');
-        category = document.createTextNode(`${key}: `);
-        const value = document.createTextNode(`${book[key]}`);
-        row.appendChild(category);
-        row.appendChild(value);
-        card.appendChild(row);
+        //is this an unsafe 
+        if(book.hasOwnProperty(key)){
+            const row = document.createElement('p');
+            row.classList.add(`${key}`);
+            category = document.createTextNode(`${key}: `);
+            const value = document.createTextNode(`${book[key]}`);
+            row.appendChild(category);
+            row.appendChild(value);
+            card.appendChild(row);
+        }
     }
     container.appendChild(card);
-}
 
-const container = document.getElementById('container')
+    const deleteButton = document.createElement('button');
+    const delText = document.createTextNode('Delete');
+    deleteButton.classList.add('delete');
+    deleteButton.appendChild(delText);
+    card.appendChild(deleteButton);
 
+    const readButton = document.createElement('button');
+    const readText = document.createTextNode('Change Read Status');
+    readButton.classList.add('read-toggle');
+    readButton.appendChild(readText);
+    card.appendChild(readButton);
 
-// for (let i = 0; i < myLibrary.length; i++){
-    
+    deleteButton.addEventListener('click', function(){
+        deleteButton.parentElement.remove();
+    });
 
-//     for (let key in myLibrary[i]) {
-//         const row = document.createElement('p');
-//         row.classList.add('info');
-//         category = document.createTextNode(`${key}: `);
-//         const value = document.createTextNode(`${myLibrary[i][key]}`);
-//         row.appendChild(category);
-//         row.appendChild(value);
-//         card.appendChild(row);
-//     }
-//     container.appendChild(card);
-// }
+    readButton.addEventListener('click', function(){
+        book.readToggle();
+        readButton.parentNode.childNodes[3].innerHTML = `Read: ${book.read}`
+    });
+};
 
-    const btn = document.createElement('button');
-    const btnText = document.createTextNode('Delete');
-    btn.appendChild(btnText);
-    card.appendChild(btn);
-// form section
+const container = document.getElementById('container');
+
+const form = document.querySelector('form');
 const showButton = document.getElementById('showForm');
 const dialog = document.getElementById("dialog");
 const cancelButton = document.getElementById('cancel');
 const submitButton = document.getElementById('submit');
-const input = document.querySelectorAll('input')
-
+const input = document.querySelectorAll('input');
+const readStatus = document.getElementById('read');
 
 showButton.addEventListener("click", function(){
     dialog.showModal();
-})
-
+});
 cancelButton.addEventListener("click", function(){
     dialog.close();
-})
-
+    document.querySelector('form').reset();
+});
 submitButton.addEventListener("click", function(event){
     event.preventDefault();
     dialog.close();
-    const newBook = new Book(`${input[0].value}`,`${input[1].value}`, `${input[2].value}`, `${input[3].value}`);
-    console.log(newBook);
-    addBookToLibrary(newBook);
-    console.log(myLibrary);
-})
+    
+    if(readStatus.checked){
+        input[3].value = 'yes'
+    }
 
+    const newBook = new Book(`${input[0].value}`,`${input[1].value}`, `${input[2].value}`, `${input[3].value}`);
+    addBookToLibrary(newBook);
+
+    document.querySelector('form').reset();  
+});
 
 const Lolita = new Book('Lolita','Vladimir Nabokov', '336', 'no')
 addBookToLibrary(Lolita);
